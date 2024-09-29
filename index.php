84052d9e430af18431258ce31e29f37e86fb4b7d
Hashara13 <hnethmin13@gmail.com>
2024-09-29 10:57:28 +0530

    style changes

--- index.php
+++ index.php
@@ -24,14 +24,14 @@
 		<div id="allTheThings">
 			<div id="member">
 				<a href="member">
-					<img src="img/ic_membership.svg" width="250px" height="auto"/><br />
+					<img src="img/pngwing.com.png" width="250px" height="auto"/><br />
 					&nbsp;Member Login
 				</a>
 			</div>
 			<div id="verticalLine">
 				<div id="librarian">
 					<a id="librarian-link" href="librarian">
-						<img src="img/ic_librarian2.svg" width="250px" height="220" /><br />
+						<img src="img/pngwing.com(1).png" width="250px" height="220" /><br />
 						&nbsp;&nbsp;&nbsp;Librarian Login
 					</a>
 				</div>
